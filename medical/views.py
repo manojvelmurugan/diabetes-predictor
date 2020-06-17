44eb266f65e23d4a3e308bab13dc2a7bdb8f8c73
manoj <manojvelmurugan@gmail.com>
2020-06-17 13:00:26 +0530

    First commit

---
+++ medical/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import requests
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+def home(request):
+    return render(request,'home.html')
+def test(request):
+    return render(request,'test.html')
+def calc(request):
+    ls = [ ] 
+    ls.append(int(request.GET['glucoselevel']))
+    ls.append(int(request.GET['Bloodpressure']))
+    ls.append(int(request.GET['Skinthickness']))
+    ls.append(int(request.GET['Insulinlevel']))
+    ls.append(float(request.GET['BMI']))
+    ls.append(float(request.GET['Diabetespedigreefunction']))
+    ls.append(int(request.GET['Age']))
+    data = pd.read_csv(r'static\dataset\diabetesample_input.csv')
+    x = data.iloc[:,[1,2,3,4,5,6,7]]
+    y = data.iloc[:,[8]]
+
+    model = KNeighborsClassifier()
+
+    model.fit(x,y.values.ravel())
+
+    pred = model.predict([ls])
+
+
+    if pred == [1]:
+        return render(request,'havedia.html')
+    else:
+        return render(request,'havenodia.html')
+
+    
